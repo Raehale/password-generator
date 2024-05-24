4408d5e8cd6019083f1a2a1ce0c63f9dfbb09a03
Raehale <87107302+Raehale@users.noreply.github.com>
2024-05-24 16:27:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+/***CREATES PASSWORD ***/
 
 function generatePasswords() {
     let passwordOne = "";
     let passwordTwo = "";
-    let passLength = document.getElementById("password_length");
+    let passwordLength = 15;
+    const passLength = document.getElementById("password_length").value;
+    const symbols = document.getElementById("symbols_answer");
+    const numbers = document.getElementById("numbers_answer");
+
     
     if (passLength) {
-        
+        passwordLength = passLength
     }
     
-    for (let i = 0; i < 15; i++) {
+    if (!symbols.checked && numbers.checked) {
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    } else if (!numbers.checked && symbols.checked){
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+    } else if (!numbers.checked && !symbols.checked){
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    }
+       
+    for (let i = 0; i < passwordLength; i++) {
         let characterOne = characters[Math.floor(Math.random() * (characters.length - 1))];
         passwordOne += characterOne;
         
@@ -24,8 +38,8 @@ function generatePasswords() {
 
 /***LIGHT MODE ***/
 
-let lightModeCheckbox = document.getElementById("light_mode");
-let container = document.getElementById("info_container");
+const lightModeCheckbox = document.getElementById("light_mode");
+const container = document.getElementById("info_container");
 
 lightModeCheckbox.addEventListener('change', function() {
   if (this.checked) {
@@ -34,3 +48,12 @@ lightModeCheckbox.addEventListener('change', function() {
     container.classList.remove("light-mode");
   }
 });
+
+/***COPY TEXT -- WIP ***/
+
+function copyOnClick() {
+    passwordOne = document.getElementById('password_field_one').textContent;
+    // passwordOne.select();
+    // document.execCommand("copy");
+    setClipboard(passwordOne);
+}
