1a99ee910374f514f4f6e4f2c7bf1940af695681
Raehale <87107302+Raehale@users.noreply.github.com>
2024-05-24 16:50:40 -0600

    changed to passwordOneText

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ function copyOnClick() {
       // Get the text field
 
   // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
+  passwordOneText.select();
+  passwordOneText.setSelectionRange(0, 99999); // For mobile devices
 
    // Copy the text inside the text field
   navigator.clipboard.writeText(passwordOneText.textContent);
